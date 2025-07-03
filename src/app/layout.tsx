202059b1fffe977b6a92a20af785872e289d1fe1
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2025-07-03 21:28:50 +0700

    update title on layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Wahyu | Portfolio",
+  title: "Wahyu Hidayat",
   description: "Computer Science Student | AI Enthusiast | Developer",
 };
 
